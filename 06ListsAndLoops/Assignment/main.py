b5e47aea190b9eca8be6bd213ca7404f4be46a45
Nola Olien <25nolaolien@usd266.com>
2023-12-04 10:27:01 -0600

    completed up to increasing function

--- 06ListsAndLoops/Assignment/main.py
+++ 06ListsAndLoops/Assignment/main.py
@@ -67,3 +67,71 @@ print(all_true(["False", "False", "False"]))
 print(all_true(["True", "True", "True"]))
 print(all_true(["True", "True", "False"]))
     
+def any_true(item):
+    variable1 = item[0]
+    variable2 = item[1]
+    variable3 = item[2]
+    for variable in item:
+        if variable1 == "True":
+            return True
+        elif variable2 == "True":
+            return True
+        elif variable3 == "True":
+            return True
+        else:
+            return False
+        
+print("Are any values true?")
+print(any_true(["True", "True", "True"]))
+print(any_true(["False", "False", "False"]))
+print(any_true(["False", "True", "False"]))
+print(any_true(["False", "False", "True"]))
+print(any_true(["True", "False", "False"]))
+
+def has_vowel(letters):
+    for letter in letters:
+        if letter in ["a", "e", "i", "o", "u"]:
+            return True
+    return False
+
+print("Does list have vowel?")      
+print(has_vowel(["l", "f", "j", "k", "i"]))
+print(has_vowel(["f", "f", "j", "g", "m"]))
+print(has_vowel(["a", "f", "q", "p", "l"]))
+print(has_vowel(["l", "m", "n", "q", "h"]))
+print(has_vowel(["a", "b", "c", "d", "e"]))
+
+def all_the_same(integer):
+    item1 = integer[0]
+    item2 = integer[1]
+    item3 = integer[2]
+    for item in integer:
+        if item1 == item2 and item2 == item3:
+            return True
+    return False
+
+print("Are all the number in the list the same?")
+print(all_the_same([5, 10, 15, 20]))
+print(all_the_same([5, 5, 5, 5]))
+print(all_the_same([6, 6, 6, 6]))
+print(all_the_same([1, 2, 4, 5]))
+print(all_the_same([7, 7, 7, 7]))
+
+def increasing(integer):
+    item1 = integer[0]
+    item2 = integer[1]
+    item3 = integer[2]
+    for item in integer:
+        if item2 > item1 and item3 > item2:
+            return True
+    else:
+        return False
+    
+print("Is the line increasing?")
+print(increasing([1, 2, 3]))
+print(increasing([4, 5, 6]))
+print(increasing([5, 6, 3]))
+print(increasing([1, 4, 2]))
+print(increasing([10, 11, 12]))
+
+
