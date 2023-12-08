550a4ecb7a04f6a8b945e87d78613a8a4a7bdc58
Nola Olien <25nolaolien@usd266.com>
2023-12-08 10:23:47 -0600

    number_guesser completed

--- 08WhileLoopsForUserInput/Assignment/main.py
+++ 08WhileLoopsForUserInput/Assignment/main.py
@@ -1,9 +1,20 @@
 import random
-def number_guesser(integer):
-    if integer == random.randint(1,10):
-        return "Good Job!"
-    elif integer > random.randint(1,10):
-        return "Too high"
-    elif integer < random.randint(1,10):
-        return "Too low"
+def number_guesser():
+    secret_number = random.randint(1, 10)
+    guess = ""
+    while guess != secret_number:
+        print("Type a random number between 1 and 10!")
+        guess = int(input())
+        
+        if guess == secret_number:
+            print("Good Job!")
+        elif guess > secret_number:
+            print("Too high")
+        elif guess < secret_number:
+            print("Too low")
+
+number_guesser()
+    
+    
+
     
