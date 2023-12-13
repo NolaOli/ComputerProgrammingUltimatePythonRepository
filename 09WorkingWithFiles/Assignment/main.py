eaf37bc59ac3cb840fc3c4d19e075db53e2bed28
Nola Olien <25nolaolien@usd266.com>
2023-12-13 10:08:23 -0600

    DONT UNDERSTAND.

--- 09WorkingWithFiles/Assignment/main.py
+++ 09WorkingWithFiles/Assignment/main.py
@@ -11,34 +11,33 @@ def count_vowels(word):
 f = open("09WorkingWithFiles/data/4000-most-common-english-words.csv", "r")
 words = f.read().split("\n")
 def most_vowels(wordlist):
-    vowel_count = 0
-    vowel_max = 0
+    count = 0
+    curr_count = 0
     for word in wordlist:
-        if letter in "aeiou":
-        # count the number of vowels in the current word
-        cur_count = count_vowels(word)
-
+        for letter in word:
+            if letter in "aeiou":
+                count = count + 1
+                curr_count = count_vowels(word)
+                if count_vowels(word) > curr_count:
+                    curr_count = count_vowels(word)
+            #return word, count_vowels(word)
         # decide whether the current word has more vowels than anything we've seen before
         # if so, it becomes the new winner!
-
-
-    
 print("The word with the most vowels is :", most_vowels(words))
-
 
 f.close()
 
 
-#import csv
+import csv
 
-#f = open("09WorkingWithFiles/data/4000-most-common-english-words.csv", "r")
-#words = f.read().split("\n")
-#length_of_words = 0
-#amount_of_words = 0
-#for row in words:
-    #amount_of_words + 1
-    #length = int(len(words))
-    #length of words 
+f = open("09WorkingWithFiles/data/4000-most-common-english-words.csv", "r")
+words = f.read().split("\n")
+length_of_words = 0
+amount_of_words = 0
+for row in words:
+    amount_of_words + 1
+    length = int(len(words))
+    length
 
 
 
