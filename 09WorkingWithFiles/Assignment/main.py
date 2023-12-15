a7d03c8a2969e55ef729250e4a1dddc18877c9a4
Nola Olien <25nolaolien@usd266.com>
2023-12-15 10:20:29 -0600

    completed count_vowels and avg_word_length

--- 09WorkingWithFiles/Assignment/main.py
+++ 09WorkingWithFiles/Assignment/main.py
@@ -1,29 +1,26 @@
 import csv 
 
 def count_vowels(word):
-    count = 0
     vowels = 0
     for letter in word:
         if letter in "aeiou":
             vowels = vowels + 1
-    return count + vowels
+    return vowels
 
 f = open("09WorkingWithFiles/data/4000-most-common-english-words.csv", "r")
 words = f.read().split("\n")
+
 def most_vowels(wordlist):
-    count = 0
-    curr_count = 0
+    mostVowels = 0
+    wordWithMostVowels = ""
     for word in wordlist:
-        for letter in word:
-            if letter in "aeiou":
-                count = count + 1
-                curr_count = count_vowels(word)
-                if count_vowels(word) > curr_count:
-                    curr_count = count_vowels(word)
-            #return word, count_vowels(word)
-        # decide whether the current word has more vowels than anything we've seen before
-        # if so, it becomes the new winner!
-print("The word with the most vowels is :", most_vowels(words))
+        curr_count = count_vowels(word)
+        if curr_count > mostVowels:
+            mostVowels = curr_count
+            wordWithMostVowels = (word)
+    #return wordWithMostVowels, mostVowels
+                
+#print("The word with the most vowels is :", most_vowels(words))
 
 f.close()
 
@@ -31,13 +28,22 @@ f.close()
 import csv
 
 f = open("09WorkingWithFiles/data/4000-most-common-english-words.csv", "r")
-words = f.read().split("\n")
-length_of_words = 0
-amount_of_words = 0
-for row in words:
-    amount_of_words + 1
-    length = int(len(words))
-    length
+wordlist = f.read().split("\n")
+total_length = 0
+word_amount = 0
+
+for word in wordlist:
+    if len(words) > 0:
+        total_length = len(word) + total_length
+        word_amount = word_amount + 1
+avg_length = total_length / word_amount
+
+print("The average wordlength of the list is:", avg_length, "letters.")
+
+f.close()
+
+
+        
 
 
 
